c68835a9d1b84bb1de4361dbfc7b018d730fe267
APERTURE17 <1538902396@qq.com>
2019-12-06 00:15:29 +0800

    c

---
+++ Project1118/Project1118/源.c
@@ -0,0 +1,35 @@
+//1118
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//	int arr[] = { 9, 5, 2, 7 };
+//	for (int i = 0; i < 4; i++)	{
+//		printf("%p\n", &arr[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//网站开发四件套LAMP
+//Linux
+//Apache(HTTP服务器)/Nginx
+//MySQL
+//PHP   Java/Python/Go
+
+//七周七语言
+
+
+//JetBrains全家桶
+//edu邮箱
+
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+//
+//	system("pause");
+//	return 0;
+//}
